71909b8b342c28b28286816b53fd4ba7afa968d3
puddo <davifpuddo@gmail.com>
2026-03-27 12:38:55 -0300

    Better error handling + option to open the waybar when there is no window open

--- src/window_detect.rs
+++ src/window_detect.rs
@@ -58,6 +58,37 @@ fn toggle_waybar(raw_pid: i32) {
     }
 }
 
+// Returns the number of active windows in the current workspace using IPC
+fn get_workspace_windows(socket_path : &String) -> i32 {
+    if let Ok(mut stream) = UnixStream::connect(socket_path) { // Connection is closed
+                                                               // automatically
+        if stream.write_all(b"activeworkspace").is_ok() {
+            let mut buffer = String::new();
+            if stream.read_to_string(&mut buffer).is_ok() {
+                let option = buffer.split("\n").nth(2);
+                match option { 
+                    Some(str) => {
+                        let str : String = str.chars()
+                                              .skip_while(|&c| !c.is_numeric())
+                                              .collect();
+                        return match str.trim().parse::<i32>() {
+                            Ok(windows) => windows,
+                            Err(e) => { 
+                                eprintln!("Could not parse the string!\nError: {}", e);
+                                0 
+                            }
+                        }
+                    },
+                    None => eprintln!("Could not retrieve the number of active windows from the String!")
+                } 
+            } 
+            else { eprintln!("Data retrieve over socket failed!"); }
+        } 
+        else { eprintln!("Data transfer over socket failed!"); }
+    } 
+    else { panic!("Could not connect to the socket!"); }
+    0
+}
 // Returns the current cursors's Y position using IPC
 fn get_pos(socket_path : &String) -> i32 {
     if let Ok(mut stream) = UnixStream::connect(socket_path) { // Connection is closed
@@ -105,21 +136,36 @@ fn main() {
     if pid != 0 {
         toggle_waybar(pid);
         let mut ypos = get_pos(&socket_path);  
+        let mut open = false;
 
         loop {
+            let windows = get_workspace_windows(&socket_path);
             let mut new_ypos = get_pos(&socket_path);
             let vel = ypos - new_ypos;
 
-            if (vel > VEL_THRESHOLD) && (new_ypos < POS_THRESHOLD) {
-                toggle_waybar(pid);
-                while new_ypos < POS_THRESHOLD {
-                    new_ypos = get_pos(&socket_path);
-                    sleep(Duration::from_millis(SLEEP_TIME));
+            // Open when no window is active
+            if windows == 0 {
+                if !open {
+                    toggle_waybar(pid);
+                    open = true;
                 }
-                toggle_waybar(pid);
+            } else {
+                    if open {
+                        toggle_waybar(pid);
+                        open = false;
+                    }
+                if (vel > VEL_THRESHOLD) && (new_ypos < POS_THRESHOLD) {
+                    toggle_waybar(pid);
+                    while new_ypos < POS_THRESHOLD {
+                        new_ypos = get_pos(&socket_path);
+                        sleep(Duration::from_millis(SLEEP_TIME));
+                    }
+                    toggle_waybar(pid);
+                    open = false;
+                }
+                ypos = new_ypos;
+                sleep(Duration::from_millis(SLEEP_TIME));
             }
-            ypos = new_ypos;
-            sleep(Duration::from_millis(SLEEP_TIME));
         }
     } else {
         eprintln!("Invalid PID detected. Waybar process could not be found!");
